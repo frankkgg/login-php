18764f0fe6976fb476560272268696a0231f7c41
frankkgg <frangrc95@gmail.com>
2023-05-12 15:45:12 -0400

    redirije cuando error

--- database.php
+++ database.php
@@ -7,7 +7,11 @@ $database = 'login';
 try {
     $cone = new PDO("mysql:host=$server;dbname=$database;", $username, $password);
     } catch (PDOException $e) {
-        die('Connection failed: '.$e->getMessage());
+        //die('Connection failed: '.$e->getMessage());
+        $message = 'Sorry there must have been an exception creating your account';
+
+         header("Location:signup.php?message=" . $message);
+
 
     }
 
